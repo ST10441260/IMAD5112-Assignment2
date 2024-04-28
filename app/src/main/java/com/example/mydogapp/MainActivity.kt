aa77286f5ef373396177636f370f04ca961742cd
CCM Lenovo <moloitmiso1@gmail.com>
2024-04-28 18:38:02 +0200

    semi commit

---
+++ app/src/main/java/com/example/mydogapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mydogapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // declaring the code
+        val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
+        btnGetStarted.setOnClickListener {
+            val intent = Intent (this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
